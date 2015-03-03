2ea4a3235026f7ac40b387809205b2b6d135dd42
Tobias Wright <tobias.wright@gmail.com>
2015-03-03 16:51:08 -0500

    more experiments with react

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,135 +1,48 @@
-
-
-
 var ArticleBox = React.createClass({
   render: function() {
     return (
-      <ArticleList timesData={this.props.timesData} />
+      <ArticleList data={this.props.data} />
     );
   }
 });
+
 var converter = new Showdown.converter();
+
 var Article = React.createClass({
   render: function() {
-    var rawMarkup = converter.makeHtml(this.props.children.toString());
+    console.log(this.props)
+
     return (
-      <div className="comment">
-        <h2 className="commentAuthor">
-          {this.props.author}
-        </h2>
-        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
-      </div>
+        <li id={this.props.assetID}>
+          <p>{this.props.title}</p>
+          <p>{this.props.byline}</p>
+          <p>{this.props.children}</p>
+        </li>
     );
   }
 });
 
 var ArticleList = React.createClass({
   render: function() {
-  	console.log(this.props.timesData)
-    var commentNodes = this.props.timesData.results.map(function (results) {
+    console.log(this.props.data.results)
+    var commentNodes = this.props.data.results.map(function (results) {
       return (
-        <Article author={results.author}>
-          {results.title}
+        <Article assetID={results.asset_id}
+          title={results.title}
+          byline={results.byline}>
+            {results.abstract}
         </Article>
       );
     });
     return (
-      <div className="articleList">
+      <ul className="articleList">
         {commentNodes}
-      </div>
+      </ul>
     );
   }
 });
 
 React.render(
-  <ArticleBox timesData={timesData}/>,
+  <ArticleBox data={timesData}/>,
   document.getElementById('content')
 );
-
-
-// var nykey = "nytfeed.php";
-
-// var buildImage = function(reseultURL) {
-// 	var j = 0,
-// 		newPathname = "";
-
-// 	imgurl = reseultURL.url;
-// 	imgurl = imgurl.split('/');
-// 	imgurl.splice(imgurl.length-1, 2);
-
-// 	for ( j = 0; j < imgurl.length; j++ ) {
-
-// 		if (j === imgurl.length-1){
-// 			newPathname += imgurl[j];
-// 			newPathname += "/";
-// 			newPathname += imgurl[j];
-// 			newPathname += "-superjumbo.jpg"
-// 		} else {							
-// 			newPathname += imgurl[j];
-// 			newPathname += "/";							
-// 		}
-// 	}
-
-// 	return newPathname;
-// }
-
-
-// $().ready(function(){
-// 	$.getJSON( nykey , function(data) {
-// 		//console.log(data)
-
-// 		$.each(data.results, function(i){
-// 			var imgurl,
-// 				newPathname,
-// 				imgcontent = "",
-// 				result = data.results[i];
-			
-// 			if (
-// 				result.media !== '' &&
-// 				result.section !== "Opinion" &&
-// 				typeof result.media[0]["media-metadata"][1] !== "undefined" &&
-// 				typeof result.media !== "undefined"
-// 			)
-// 			{	
-
-// 				newPathname = buildImage(result.media[0]["media-metadata"][1]);
-						
-
-// 				imgcontent = "<li class='article'>";
-// 				imgcontent += "<p class='byline'>" + result.section + "</p>";
-				
-// 				imgcontent += "<div id='imgdiv"+i+"' class='loading'>";
-// 				imgcontent += "<img class='hideimage' src='" + newPathname + "' id='jumbo" +i+ "'>";
-// 				imgcontent += "</div>";
-				
-// 				imgcontent += "<p><strong>" + result.title + ".</strong> ";
-// 				imgcontent += result.abstract + " ";
-// 				imgcontent += "<a href='" + result.url + "' class='readmore'>Read Story</a></p>";
-// 				imgcontent += "<p class='byline'>" + result.byline
-// 				//if (data.results[i].media[0].copyright) {"/ Photo Credit: " + data.results[i].media[0].copyright + "</p>"};
-// 				imgcontent += "</li>";
-				
-// 				$( imgcontent ).appendTo("#content");
-						
-// 			}
-
-
-// 			$('#jumbo'+i).load(function() {
-				
-// 				//centers images less then the 1024px, the common size for nyt jumbo sizes - usually a portrait
-// 				var img_width = this.width;
-				
-// 				//wanted a nice fade in despite loading time
-// 				setTimeout (function () {
-// 					$("#imgdiv"+i).removeClass("loading");
-// 					$("#jumbo"+i).fadeIn("slow").removeClass("hideimage").addClass("showImage");
-
-// 					if (img_width < 1280) {
-// 						$('#jumbo'+i).css({"width":img_width,"display":"block"})
-// 					}
-
-// 				}, 3000);
-// 			});		
-// 		});
-// 	});
-// });
