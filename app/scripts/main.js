27a2e6283d6b5617217669476c7001ea44d58ce7
Tobias Wright <tobias.wright@gmail.com>
2015-03-16 22:19:29 -0400

    reorganized data parsing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,9 @@ var ArticleBox = React.createClass({
     return {data: []};
   },
   componentDidMount: function() {
-    var that = this
+    var that = this;
+    var articles = [];
+    var results;
     var options = {
       url: 'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json',
       source: "nytimes"
@@ -13,46 +15,56 @@ var ArticleBox = React.createClass({
 
     $.getJSON( "https://radiant-bayou-8874.herokuapp.com/proxy", options, function(data){
       data = JSON.parse(data);
-      that.setState({data: data.results});
-      console.log(this, that)
+
+      results = data.results
+      results.map( function(result) {
+        var article = {};
+        if (result.section !== "Opinion" && result.media.length ) {
+          result.media[0]["media-metadata"].map(function (image) {
+            if (image.format === "superJumbo" ) {
+              article = {
+                "title":result.title,
+                "url":result.url,
+                "byline":result.byline,
+                "img":image.url
+              }
+              articles.push(article)
+            }
+          });
+        }
+      })
+      that.setState({data: articles});
     });
   },
   render: function() {
-        return (
+    return (
       <Articles data={this.state.data} />
     );
   }
 });
 
 var Captions = React.createClass({
-  
   render: function() {
-    console.log(this.props.data)
-    var rawMarkup = converter.makeHtml('###'+this.props.data.title.toString());
+    var rawMarkup = converter.makeHtml('###'+this.props.title);
     return (
       <div>
         <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
-        <p>{this.props.data.byline}</p>
+        <p>{this.props.byline}</p>
       </div>
     );
   }
 });
 
-
 var Articles = React.createClass({
   render: function() {
-    console.log("allo", this.props.data)
     var results = this.props.data;
-    console.log("allo", results)
     var commentNodes = results.map(function (result) {
-      if (result.section !== "Opinion" && result.media.length ) {
         return (
           <li>
-            <ArticleImage data={result} />
-            <Captions data={result} />
+            <ArticleImage image={result.img} />
+            <Captions title={result.title} byline={result.byline} />
           </li>
         )
-      }
     });
     return (
       <ul className="article">{commentNodes}</ul>
@@ -62,19 +74,8 @@ var Articles = React.createClass({
 
 var ArticleImage = React.createClass({
   render: function() {
-    var media = this.props.data;
-    var superImages = [];
-
-    if (media.media.length) {
-      media.media[0]["media-metadata"].map(function (format) {
-        if (format.format === "superJumbo" ) {
-          superImages.push(format.url)
-        }
-      });
-    }
-
     return (
-      <img className="img" src={superImages.map( function(img){ return img })} />
+      <img className="img" src={this.props.image} />
     )
   }
 });
